41540594c52bcf840112a5db71ff728eb5fcb2a7
Marcel <marcel.spryszynski@gmail.com>
2023-03-17 22:26:53 +0100

    Elevator system created

--- src/test/java/com/example/avsystem/model/ElevatorPickupServiceTest.java
+++ src/test/java/com/example/avsystem/model/ElevatorPickupServiceTest.java
@@ -58,16 +58,105 @@ public class ElevatorPickupServiceTest {
     }
 
     @Test
-    public void xd() {
+    public void findTheNearestElevatorFromEmptyOnes() {
         //given
         ElevatorStatus elevatorStatus1 = new ElevatorStatus(0, null, new PriorityQueue<>(), new PriorityQueue<>(), null, false, false);
         ElevatorStatus elevatorStatus2 = new ElevatorStatus(4, null, new PriorityQueue<>(), new PriorityQueue<>(), null, false, false);
-        List<Elevator> elevators = new LinkedList<>(List.of(new Elevator(2, elevatorStatus1), new Elevator(233, elevatorStatus2)));
-
+        Elevator elevator1 = new Elevator(2, elevatorStatus1);
+        Elevator elevator2 = new Elevator(233, elevatorStatus2);
+        List<Elevator> elevators = new LinkedList<>(List.of(elevator1, elevator2));
+        PickupRequest pickupRequest = new PickupRequest(3, ElevatorDirection.UP);
 
         //when
-        elevatorPickupService.findNearestPossibleElevator(elevators, )
+        Optional<Elevator> elevator = elevatorPickupService.findNearestPossibleElevator(elevators, pickupRequest);
 
         //then
+        assertEquals(Optional.of(elevator2), elevator);
+    }
+
+    @Test
+    public void findAlreadyPickedUpElevator() {
+        //given
+        ElevatorStatus elevatorStatus1 = new ElevatorStatus(0, null, new PriorityQueue<>(), new PriorityQueue<>(), null, false, false);
+        ElevatorStatus elevatorStatus2 = new ElevatorStatus(4, 6, new PriorityQueue<>(), new PriorityQueue<>(), ElevatorDirection.UP, false, false);
+        Elevator elevator1 = new Elevator(2, elevatorStatus1);
+        Elevator elevator2 = new Elevator(233, elevatorStatus2);
+        List<Elevator> elevators = new LinkedList<>(List.of(elevator1, elevator2));
+        PickupRequest pickupRequest = new PickupRequest(5, ElevatorDirection.UP);
+
+        //when
+        Optional<Elevator> elevator = elevatorPickupService.findNearestPossibleElevator(elevators, pickupRequest);
+
+        //then
+        assertEquals(Optional.of(elevator2), elevator);
+    }
+
+    @Test
+    public void skipAlreadyPickedUpElevatorBecauseWrongDirection() {
+        //given
+        ElevatorStatus elevatorStatus1 = new ElevatorStatus(0, null, new PriorityQueue<>(), new PriorityQueue<>(), null, false, false);
+        ElevatorStatus elevatorStatus2 = new ElevatorStatus(4, 6, new PriorityQueue<>(), new PriorityQueue<>(), ElevatorDirection.UP, false, false);
+        Elevator elevator1 = new Elevator(2, elevatorStatus1);
+        Elevator elevator2 = new Elevator(233, elevatorStatus2);
+        List<Elevator> elevators = new LinkedList<>(List.of(elevator1, elevator2));
+        PickupRequest pickupRequest = new PickupRequest(5, ElevatorDirection.DOWN);
+
+        //when
+        Optional<Elevator> elevator = elevatorPickupService.findNearestPossibleElevator(elevators, pickupRequest);
+
+        //then
+        assertEquals(Optional.of(elevator1), elevator);
+    }
+
+    @Test
+    public void skipAlreadyPickedUpElevatorBecauseItRequiresGoingBack() {
+        //given
+        ElevatorStatus elevatorStatus1 = new ElevatorStatus(0, null, new PriorityQueue<>(), new PriorityQueue<>(), null, false, false);
+        ElevatorStatus elevatorStatus2 = new ElevatorStatus(5, 6, new PriorityQueue<>(), new PriorityQueue<>(), ElevatorDirection.UP, false, false);
+        Elevator elevator1 = new Elevator(2, elevatorStatus1);
+        Elevator elevator2 = new Elevator(233, elevatorStatus2);
+        List<Elevator> elevators = new LinkedList<>(List.of(elevator1, elevator2));
+        PickupRequest pickupRequest = new PickupRequest(4, ElevatorDirection.UP);
+
+        //when
+        Optional<Elevator> elevator = elevatorPickupService.findNearestPossibleElevator(elevators, pickupRequest);
+
+        //then
+        assertEquals(Optional.of(elevator1), elevator);
+    }
+
+    @Test
+    public void skipAlreadyPickedUpElevatorBecauseItDoesntMatchDirectionToPickupFloor() {
+        //given
+        ElevatorStatus elevatorStatus1 = new ElevatorStatus(0, null, new PriorityQueue<>(), new PriorityQueue<>(), null, false, false);
+        ElevatorStatus elevatorStatus2 = new ElevatorStatus(10, 6, new PriorityQueue<>(), new PriorityQueue<>(), ElevatorDirection.UP, false, false);
+        Elevator elevator1 = new Elevator(2, elevatorStatus1);
+        Elevator elevator2 = new Elevator(233, elevatorStatus2);
+        List<Elevator> elevators = new LinkedList<>(List.of(elevator1, elevator2));
+        PickupRequest pickupRequest = new PickupRequest(7, ElevatorDirection.UP);
+
+        //when
+        Optional<Elevator> elevator = elevatorPickupService.findNearestPossibleElevator(elevators, pickupRequest);
+
+        //then
+        assertEquals(Optional.of(elevator1), elevator);
+    }
+
+
+    @Test
+    public void findNoneElevator() {
+        //given
+        ElevatorStatus elevatorStatus1 = new ElevatorStatus(0, 5, new PriorityQueue<>(), new PriorityQueue<>(), ElevatorDirection.DOWN, false, false);
+        ElevatorStatus elevatorStatus2 = new ElevatorStatus(10, 6, new PriorityQueue<>(), new PriorityQueue<>(), ElevatorDirection.UP, false, false);
+        Elevator elevator1 = new Elevator(2, elevatorStatus1);
+        Elevator elevator2 = new Elevator(233, elevatorStatus2);
+        List<Elevator> elevators = new LinkedList<>(List.of(elevator1, elevator2));
+        PickupRequest pickupRequest = new PickupRequest(7, ElevatorDirection.UP);
+
+        //when
+        Optional<Elevator> elevator = elevatorPickupService.findNearestPossibleElevator(elevators, pickupRequest);
+
+        //then
+        assertEquals(Optional.empty(), elevator);
     }
 }
